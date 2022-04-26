4a3e1b9a22e49058ffadb3a4db2e01b04b8bd982
Ma Wenxi <wenxi.ma@nju.edu.cn>
2022-04-26 20:27:46 +0800

    Adding annotations to client.c and serv.c

--- examples/tcp/client.c
+++ examples/tcp/client.c
@@ -21,19 +21,20 @@ int main(int argc, char *argv[])
 		fputs("usage: ./client message\n", stderr);
 		exit(1);
 	}
+/* Pass the input string argument */  
 	str = argv[1];
-    
+/* Initialize the clientfd */    
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
+/* Specify the server address */    
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
 	servaddr.sin_port = htons(SERV_PORT);
-    
+/* Connect to server */     
 	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-
+/* Send msg from string */ 
 	write(sockfd, str, strlen(str));
-
+/* Receive server response and print to stdio*/ 
 	n = read(sockfd, buf, MAXLINE);
 	printf("Response from server:\n");
 	write(STDOUT_FILENO, buf, n);
